2323cf6c0065c20d4dc149bb9e7f9ffa79ebc680
안윤기 <ahnyunki@an-yungiui-MacBookAir.local>
2023-04-06 16:28:27 +0900

    트랜잭션 적용과 영속성 컨텍스트

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +23,10 @@ public class UserServiceV2 {
     /**
      * 생성 기능
      */
+    // 아래 있는 함수가 시작될 때 start transaction; 을 해준다 (트랜잭션을 시작!)
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         User u = userRepository.save(new User(request.getName(), request.getAge()));
     }
@@ -29,6 +34,7 @@ public class UserServiceV2 {
     /**
      * 조회 기능
      */
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                                 .map(UserResponse::new)
@@ -38,17 +44,18 @@ public class UserServiceV2 {
     /**
      * 업데이트 기능
      */
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
                       .orElseThrow(IllegalArgumentException::new); // user 가 없는 경우 예외가 던져진다.
 
         user.updateName(request.getName());
-        userRepository.save(user);
     }
 
     /**
      * 삭제 기능
      */
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name)
                 .orElseThrow(IllegalArgumentException::new);
